bbff77c73d9cb3eb0586784f3202adeb1fc6da50
akiyosi <akiyoshi.maruyama@gmail.com>
2018-04-19 19:56:41 +0900

    Change function to package.

--- main.go
+++ main.go
@@ -1,33 +1,35 @@
-package main
+package shortpath
 
 import (
 	"fmt"
-	"os"
 	"strings"
+        "runtime"
 
 	homedir "github.com/mitchellh/go-homedir"
 )
 
-func main() {
-	cur, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
+func Minimum(cur string) (string, error) {
 	home, err := homedir.Dir()
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	// Shorten homedir
 	cur = strings.Replace(cur, home, "~", 1)
 
-	out := strings.Split(cur, "/")
-	for i := 0; i < len(out)-2; i++ {
+        var out []string
+        if runtime.GOOS == "windows" {
+	        out = strings.Split(cur, "\\")
+        } else {
+	        out = strings.Split(cur, "/")
+        }
+
+	for i := 0; i < len(out)-1; i++ {
 		if len(out[i]) > 0 {
 			out[i] = string(out[i][0])
 		}
 	}
 
-	fmt.Print(strings.Join(out, "/"))
+	ret := fmt.Sprintf("%s", strings.Join(out, "/"))
+	return ret, nil
 }
