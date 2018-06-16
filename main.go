b8a714169624a9d596fa62db7946caa5b8e9da4f
akiyosi <akiyoshi.maruyama@gmail.com>
2018-06-16 13:54:15 +0900

    Fix.

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package shortpath
 
 import (
 	"fmt"
+	"runtime"
 	"strings"
-        "runtime"
 
 	homedir "github.com/mitchellh/go-homedir"
 )
@@ -17,20 +17,20 @@ func Minimum(cur string) (string, error) {
 	// Shorten homedir
 	cur = strings.Replace(cur, home, "~", 1)
 
-        var out []string
-        if runtime.GOOS == "windows" {
-	        out = strings.Split(cur, "\\")
-        } else {
-	        out = strings.Split(cur, "/")
-        }
+	var out []string
+	if runtime.GOOS == "windows" {
+		out = strings.Split(cur, "\\")
+	} else {
+		out = strings.Split(cur, "/")
+	}
 
-	outsc := []rune(out)
-	for i := 0; i < len(outsc)-1; i++ {
-		if len(outsc[i]) > 0 {
-			outsc[i] = string(outsc[i][0])
+	for i := 0; i < len(out)-1; i++ {
+		runeout := []rune(out[i])
+		if len(runeout) > 0 {
+			out[i] = string(runeout[0])
 		}
 	}
 
-	ret := fmt.Sprintf("%s", strings.Join(outsc, "/"))
+	ret := fmt.Sprintf("%s", strings.Join(out, "/"))
 	return ret, nil
 }
