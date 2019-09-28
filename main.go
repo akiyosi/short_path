68a4d8ef8723f4a1a2d84da926c848a036ceebfa
akiyosi <akiyoshi.maruyama@gmail.com>
2019-09-28 13:19:57 +0900

    Add PrettyMinimum()

--- main.go
+++ main.go
@@ -8,20 +8,20 @@ import (
 	homedir "github.com/mitchellh/go-homedir"
 )
 
-func Minimum(cur string) (string, error) {
+func Minimum(path string) (string, error) {
 	home, err := homedir.Dir()
 	if err != nil {
 		return "", err
 	}
 
 	// Shorten homedir
-	cur = strings.Replace(cur, home, "~", 1)
+	path = strings.Replace(path, home, "~", 1)
 
 	var out []string
 	if runtime.GOOS == "windows" {
-		out = strings.Split(cur, "\\")
+		out = strings.Split(path, "\\")
 	} else {
-		out = strings.Split(cur, "/")
+		out = strings.Split(path, "/")
 	}
 
 	for i := 0; i < len(out)-1; i++ {
@@ -33,4 +33,34 @@ func Minimum(cur string) (string, error) {
 
 	ret := fmt.Sprintf("%s", strings.Join(out, "/"))
 	return ret, nil
+}
+
+func PrettyMinimum(path string) (string, error) {
+	home, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+
+	// Shorten homedir
+	path = strings.Replace(path, home, "~", 1)
+
+	var out []string
+	if runtime.GOOS == "windows" {
+		out = strings.Split(path, "\\")
+	} else {
+		out = strings.Split(path, "/")
+	}
+
+	if len(out) <= 2 {
+		return path, nil
+	}
+
+	for i := 0; i < len(out)-1; i++ {
+		runeout := []rune(out[i])
+		if len(runeout) > 0 {
+			out[i] = string(runeout[0])
+		}
+	}
+
+	return fmt.Sprintf("%s", strings.Join(out, "/")), nil
 }
